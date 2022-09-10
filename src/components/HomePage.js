fa4ae9a8b99736188050fed637bc656503de83f6
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-10 12:18:49 +0100

    hompage component done

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../index.css";
+function HomePage(){
+    return (
+        <>
+            <p className="hello-im-Anfal">Hello,</p>
+            <p className="hello-im-Anfal">I'm Anfal
+            </p>
+            <p className="text"> Aspiring  <span className="Aspiring Software Developer p-2"></span>
+            </p>
+            </>
+              );
+            }
+            
+            export default HomePage;

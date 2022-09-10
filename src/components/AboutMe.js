1dfec50f3199a2e1e113f6f1cda5846237c866a6
aali9 <74592306+aali9@users.noreply.github.com>
2022-09-10 12:24:39 +0100

    about me and contact js

---
+++ src/components/AboutMe.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../index.css";
+
+function AboutMe() {
+  return (
+    <div className="row">
+      <div className="col-md-2 col-sm-10 mx-2 mb-3" id="projects-title">
+        <span id="about-title">About</span>
+      </div>
+      <div className="col-md-9 col-sm-12 mx-2 about-me-text">
+      I am an aspiring Software Developer who has studied a full stack development course at the University of Birmingham.
+      I am extremely keen on technology which is fuelled by my intertest and passion in coding. I am keen to explore how websites work and the intricate details of creating interactive and immersive web pages.
+        <br/><br/>
+            </div>
+    </div>
+
+  );
+}
+
+export default AboutMe;
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../index.css";
+
+export default function Contact() {
+    return (
+        <div className="contact-container">
+        <p id="contact">Contact</p>
+        <div id="information">
+            <p>Anfal Ali</p>
+            <p>email: anfal.ali_@hotmail.co.uk</p>
+            <p>GitHub: <a href="https://github.com/aali9">https://github.com/aali9</a></p>
+            <p>phone: 0762345342</p>
+        </div>
+    </div>
+    )
+}
